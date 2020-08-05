60f0c3a7198413a270d12fc728e238cf1049794e
Tram <tremmertt98@gmail.com>
2020-08-05 21:19:24 +0700

    them_chuc_nang_them_sinh_vien

--- services/SinhVienService.js
+++ services/SinhVienService.js
@@ -1,4 +1,12 @@
 var SinhVienService = function() {
+    this.themSinhVien = function(sinhVien) {
+        return axios ({
+            url: 'http://svcy.myclass.vn/api/SinhVien/ThemSinhVien',
+            method: 'POST',
+            data: sinhVien
+        })
+    }
+
     this.xoaSinhVien = function(maSV) {
         return axios ({
             url:'http://svcy.myclass.vn/api/SinhVien/XoaSinhVien/' + maSV,
